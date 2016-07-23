74e14a23777a9f966146182eaddb46e503f916fb
Ben Kalegin <ben32768@gmail.com>
2016-07-23 00:35:03 -0400

    submission 1

--- Integer Sequence Learning/scripts/Recurrence Equation Solve.R
+++ Integer Sequence Learning/scripts/Recurrence Equation Solve.R
@@ -1,21 +1,21 @@
 # x[j]   = a * x[j-1] + b * x[j-2]
 # x[j+1] = a * x[j]   + b * x[j-1]
 
-solveRecurrent <- function(x, depth, offset) {
+solveRecurrent <- function(x, depth) {
     A <- matrix(0, nrow = depth, ncol = depth)
     b <- matrix(0, nrow = depth, ncol = 1)
 
     for (r in 1:depth) { 
         for (c in 1:depth) {
-            A[r, c] <- x[offset + (r - 1) + (c-1)]
+            A[r, c] <- x[1 + (r - 1) + (c-1)]
         }
-        b[r] <- x[offset + depth + (r-1)] 
+        b[r] <- x[1 + depth + (r-1)] 
     }
     result <- try(round(solve(A, b)));
     if ("matrix" != class(result)) {
         result <- matrix(NA, nrow = depth, ncol = 1);
     }
-    t(result)
+    t(result)[1,]
 }
 
 
@@ -24,8 +24,7 @@ solveRecurrent <- function(x, depth, offset) {
 #x5 <- c(18, 24, 30, 36, 42, 54)
 #solveRecurrent(x5, depth = 2, offset = 3)
 
-isRecurrent <- function(x, depth) {
-    s <- solveRecurrent(x, depth = depth, offset = 1)
+isRecurrent <- function(x, depth, s) {
     result = TRUE
     if (!anyNA(s)) {
       for(i in (depth+1):(length(x) - depth)) {
@@ -33,7 +32,7 @@ isRecurrent <- function(x, depth) {
         for (j in 1:depth) {
           value <- value + x[i+j-1] * s[j]    
         }
-        if (is.na(x[i+depth]) || value != x[i+depth]) {
+        if (is.na(x[i+depth]) || abs(value - x[i+depth]) > 0.01 ) {
           result = FALSE
           break;
         }
@@ -42,18 +41,40 @@ isRecurrent <- function(x, depth) {
       result = FALSE
     result
 }
-
-
 #isRecurrent(x11, depth = 2)
 #isRecurrent(x12, depth = 2)
 
-train <- read.csv("../input/train.csv", stringsAsFactors = FALSE, nrow = 1000000)
-sequences <- lapply(strsplit(train$Sequence, split = ","), FUN = as.numeric)
+predictNext <- function(x, depth, s, isRecurrent) {
+    if (isRecurrent) {
+        i <- length(x) - depth + 1;
+        value <- 0
+        for (j in 1:depth) {
+            value <- value + x[i + j - 1] * s[j]
+        }
+    } else
+        value <- NA
+    value
+}
 
-rec2 <- lapply(sequences, isRecurrent, depth = 2)
-rec3 <- lapply(sequences, isRecurrent, depth = 3)
-rec4 <- lapply(sequences, isRecurrent, depth = 4)
 
+#train <- read.csv("../input/train.csv", stringsAsFactors = FALSE, nrow = 10000)
+#sequences <- lapply(strsplit(train$Sequence, split = ","), FUN = as.numeric)
 
+#rec2 <- lapply(sequences, isRecurrent, depth = 2)
+#rec3 <- lapply(sequences, isRecurrent, depth = 3)
+#system.time(rec4 <- lapply(sequences, isRecurrent, depth = 4)) # 1.7 on 10000, then 3.07 when value == x[] changed to abs(value - x) < 0.01, but records found 6120 instead of 227
 
+test <- read.csv("../input/test.csv", stringsAsFactors = FALSE, nrow = 10000000)
+test$Sequence <- sapply(strsplit(test$Sequence, split = ","), FUN = as.numeric)
+# length(testseq) == 113845
+test$Solve2 <- lapply(test$Sequence, FUN = solveRecurrent, depth = 2)
+test$Check2 <- mapply(isRecurrent, test$Sequence, 2, test$Solve2)
+# sum(test$Check2 == TRUE) 2510
+# sum(test$Check2 == FALSE) 111335
 
+#head(test[test$Check2 == TRUE,])
+
+test$Last <- mapply(predictNext, SIMPLIFY = TRUE, test$Sequence, 2, test$Solve2, test$Check2)
+test$Last[is.na(test$Last)] <- 0
+test$Last[abs(test$Last) < 1E8] <- as.integer(test$Last[abs(test$Last) < 1E8])
+write.csv(test[, c("Id", "Last")], "../output/recurrent2.csv", row.names = FALSE) # submission 1
